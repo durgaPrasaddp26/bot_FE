2c04d9b29f429e9a3e5fb5c40d269d78613961e8
durgaPrasaddp26 <saidurgaprasadsdp26@gmail.com>
2025-08-06 02:40:46 +0530

    changes 2

--- src/App.jsx
+++ src/App.jsx
@@ -325,7 +325,7 @@ export default function ChatApp() {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/chat', {
+      const res = await axios.post('https://bot-be-00pc.onrender.com/api/chat', {
         message: input,
       });
 
